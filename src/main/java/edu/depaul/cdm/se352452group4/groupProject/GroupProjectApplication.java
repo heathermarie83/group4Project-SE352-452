561764d0029b8adf46f4f14afa57ba3c60ebb641
Ross <ross.a.swanson@gmail.com>
2019-04-04 12:23:17 -0500

    Initial commit

---
+++ src/main/java/edu/depaul/cdm/se352452group4/groupProject/GroupProjectApplication.java
@@ -0,0 +1,14 @@
+package edu.depaul.cdm.se352452group4.groupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectApplication.class, args);
+		System.out.println("SE 352/452 Group 4 project running on port 8080");
+	}
+
+}
